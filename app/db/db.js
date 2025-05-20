9b3cb846c91db44b6ecb93a4c2eb6f4980fc336f
Rohan <rohan.k.sah@gmail.com>
2025-05-20 18:41:45 -0500

    lets see

--- app/db/db.js
+++ app/db/db.js
@@ -1,12 +1,12 @@
 import { MongoClient, ServerApiVersion } from "mongodb"
-const uri = "mongodb+srv://rohanksah:mIVbewWjQ5HbROWN@leetcodecluster.krk2dri.mongodb.net/?retryWrites=true&w=majority&appName=LeetcodeCluster";
+const uri = process.env.MONGODB_URI || "mongodb://rohansah:mIVbewWjQ5HbROWN@host:27017/dbname";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,
+    deprecationErrors: true
   }
 });
 
@@ -14,7 +14,6 @@ export async function getProblemsCollection() {
   if (!client.topology || !client.topology.isConnected()) {
     await client.connect();
   }
-  // Use a single database, e.g., "leetcode"
   return client.db("leetcode").collection("problems")
 }
 
