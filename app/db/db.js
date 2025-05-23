d15d246e3cd994bc9f70ba5a18d0e52d12561219
Rohan <rohan.k.sah@gmail.com>
2025-05-22 21:04:02 -0500

    feat added tls

--- app/db/db.js
+++ app/db/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb"
 const uri = process.env.MONGO_URI || "mongodb+srv://rohanksah:mIVbewWjQ5HbROWN@leetcodecluster.krk2dri.mongodb.net/";
 
 // Use default options for Atlas
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, { tls: true });
 
 export async function getProblemsCollection() {
   if (!client.topology || !client.topology.isConnected()) {
